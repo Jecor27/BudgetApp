a934d9f0a4a2106854104a316bfb7964f10061fe
Jesus <jjcorrales76@gmail.com>
2025-05-04 19:23:46 -0600

    added paths and elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//Layouts
+import Main, { mainLoader } from "./layouts/Main";
+
+// Actions
+import { logoutAction } from "./actions/logout";
 
 // Routes
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
@@ -8,16 +13,30 @@ import Error from "./pages/Error";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
-    loader: dashboardLoader,
-    errorElement: <Error />
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
   },
 ]);
 
 function App() {
-  return <div className="App">
-    <RouterProvider router={router} />
-  </div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
